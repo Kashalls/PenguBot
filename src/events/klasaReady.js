b51a9f5629f1c20e1c851adc00de81aa727a520a
Jack <jacknagle22@gmail.com>
2019-04-03 06:31:47 +1100

    fix

--- src/events/klasaReady.js
+++ src/events/klasaReady.js
@@ -5,7 +5,10 @@ module.exports = class extends Event {
 
     async run() {
         // Setup lavalink
-        this.client.lavalink = new MusicClient(this.client, this.client.config.nodes, {});
+        this.client.lavalink = new MusicClient(this.client, this.client.config.nodes, {
+            shards: this.client.shard.shardCount,
+            user: this.client.user.id
+        });
         this.client.console.log(`[${this.client.shard.id}]: Online`);
     }
 
