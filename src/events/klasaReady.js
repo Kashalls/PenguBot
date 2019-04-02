411480eeea017ea654d74aac0d6ddbf8c53c53cd
Jack <jacknagle22@gmail.com>
2019-04-03 06:18:56 +1100

    Quick fix for now

--- src/events/klasaReady.js
+++ src/events/klasaReady.js
@@ -5,7 +5,7 @@ module.exports = class extends Event {
 
     async run() {
         // Setup lavalink
-        this.client.lavalink = new MusicClient(this.client, this.client.config.nodes);
+        this.client.lavalink = new MusicClient(this.client, this.client.config.nodes, {});
         this.client.console.log(`[${this.client.shard.id}]: Online`);
     }
 
